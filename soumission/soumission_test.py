c667c6d280338d361839cca2363d4140921fd8fe
Pouré Thomas <thomas@MacBook-Air-de-Poure.local>
2025-11-04 09:27:28 +0100

    exectuable générant une prediction completement aléatoire

---
+++ soumission/soumission_test.py
@@ -0,0 +1,15 @@
+import csv
+import random
+    
+# Nom du fichier à créer
+filename = "prediction.csv"
+# Création du fichier CSV
+with open(filename, mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["ID", "TARGET"])  # En-têtes
+    
+    start_id = 225000
+    for i in range(75000):
+        writer.writerow([start_id + i, random.randint(0, 1)])  # 0 ou 1 aléatoire
+    
+print(f"Fichier '{filename}' créé avec succès !")
