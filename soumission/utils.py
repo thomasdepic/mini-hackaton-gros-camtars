ab6da7866bb26400b6234bfbb2c10180d0a060a5
Pouré Thomas <thomas@MacBook-Air-de-Poure.local>
2025-11-04 13:18:47 +0100

    Ajout d'une entre, les indexes, qui n'ont donc plus besoin d'être ajouté à l'entre des features

--- soumission/utils.py
+++ soumission/utils.py
@@ -1,8 +1,19 @@
 import csv
 
-def soumission_csv(model,df_data,filename="../data/prediction.csv"):
-
-    indexes = df_data["ID"]
+def soumission_csv(model,df_data,indexes,filename="../data/prediction.csv"):
+    """
+    Génère un fichier csv de predictions reponse à soumettre à partir d'un model entrainé et d'un data_frame.
+        
+    Paramètres :
+    - model, model entrainé 
+    - df_data : data frame des données à predir, dois contenir la colone "ID" et les meme features que le model entrainé
+    - filename : nom du fichier créé par la fonction
+        
+    Retour :
+    - X_train_pca, X_test_pca : listes de matrices transformées
+    - pca_list : liste des objets PCA par état
+    - top_features_dict : dict avec top features par état
+    """
     features = df_data.drop(columns=["ID"], errors='ignore')
 
     prediction = model.predict(features)
